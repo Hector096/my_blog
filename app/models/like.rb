5d0c38a1410b15a23ab925a3ba3b14ea27bb24de
Hector <vv96rca@gmail.com>
2022-01-24 22:44:03 +0530

    resolve linter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,10 @@
 class Like < ApplicationRecord
-    belongs_to  :author,class_name: 'User'
-    belongs_to :posts
+  belongs_to :author, class_name: 'User'
+  belongs_to :posts
 
-    after_save :update_likes
+  after_save :update_likes
 
-    def update_likes
-        post.increment!(:likes_counter)
-    end
-    
+  def update_likes
+    post.increment!(:likes_counter)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ApplicationRecord
- has_many :posts,foreign_key: 'author_id'
- has_many :likes,foreign_key: 'author_id'
- has_many :comments,foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
 
-
- def latest_posts
-
-    return post.limit(3).order(created_at: :asc)
- end
-
+  def latest_posts
+    post.limit(3).order(created_at: :asc)
+  end
 end
