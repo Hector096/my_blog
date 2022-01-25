b0a5fce53e4585d60d1111724536b4a2c6b02454
Hector <vv96rca@gmail.com>
2022-01-25 11:15:32 +0530

    resolve linter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
-belongs_to :author, class_name 'User'
-has_many :likes
-has_many : comments
-after_save :update_post
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+  after_save :update_post
 
-def update_post
+  def update_post
     author.posts.increment!(:posts_counter)
-end
-   
-def five_recent_comments
+  end
+
+  def five_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 end
