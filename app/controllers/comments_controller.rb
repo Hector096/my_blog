8262e391546058703f5240571432e682abdf4923
Hector <vv96rca@gmail.com>
2022-01-28 16:51:41 +0530

    Add comment and likes controller and views

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(text: comment_params[:text], author_id: current_user.id, post_id: @post.id)
+
+    respond_to do |format|
+      format.html do
+        redirect_to user_post_path(@post.author.id, @post.id) if @comment.save
+      end
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,27 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    post = current_user.posts.new
+    render :new, locals: { post: post }
+  end
+
+  def create
+    post = current_user.posts.new(params.require(:post).permit(:title, :text))
+    @user = current_user
+    @posts = @user.posts
+
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:success] = 'Post saved successfully'
+          redirect_to user_posts_url
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          render :new
+        end
+      end
+    end
+  end
 end
